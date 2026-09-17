7483b6e02db6b5ec9a0b897526e103baa6d62bdd
Nurdos <kanalbekovnurdos@gmail.com>
2026-09-17 16:31:40 +0500

    initial commit

---
+++ SIS1/Part1/5.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} rowsCount
+ * @param {number} colsCount
+ * @return {Array<Array<number>>}
+ */
+Array.prototype.snail = function (rowsCount, colsCount) {
+  if (rowsCount * colsCount !== this.length) {
+    return [];
+  }
+
+  const result = Array.from({ length: rowsCount }, () => []);
+
+  let index = 0;
+
+  for (let col = 0; col < colsCount; col++) {
+    if (col % 2 === 0) {
+      for (let row = 0; row < rowsCount; row++) {
+        result[row][col] = this[index++];
+      }
+    } else {
+      for (let row = rowsCount - 1; row >= 0; row--) {
+        result[row][col] = this[index++];
+      }
+    }
+  }
+
+  return result;
+};
